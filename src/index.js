9beb038d900259b2f0306ad5bf65464d11309a18
Sierra-034 <sierra_034@outlook.com>
2021-03-04 23:36:03 -0600

    107: useEffect - SimpleForm

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { HookApp } from './HookApp';
-import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimmpleForm';
 
 ReactDOM.render(
-  <CounterWithCustomHook/>,
+  <SimpleForm/>,
   document.getElementById('root')
 );
