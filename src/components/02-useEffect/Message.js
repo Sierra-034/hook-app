d0736edb9b7bbd9d72b9fbec98c00769e176130a
Sierra-034 <sierra_034@outlook.com>
2021-03-05 00:22:56 -0600

    109: useEffect - Precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({
+        x: 0,
+        y: 0
+    });
+
     useEffect(() => {
         console.log("Componente montado");
+        const mouseMove = (event) => {
+            const coords = {x: event.x, y: event.y};
+            setCoords(coords);
+        };
+
+        window.addEventListener("mousemove", mouseMove);
+
         return () => {
             console.log("Componente desmontado");
-        }
-    });
+            window.removeEventListener("mousemove", mouseMove);
+        };
+    }, []);
     
     return (
         <div>
             <h3>Hola sierra-034</h3>
+            <code>x: {coords.x}, y: {coords.y}</code>
         </div>
     );
 };
