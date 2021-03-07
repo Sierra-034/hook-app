777edccf30dcc92eef5647876209c4694240b626
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-03-07 15:41:27 -0600

    110: Formulario con custom Hook

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Message } from './Message';
+import './effects.css';
 
 export const SimpleForm = () => {
     const [formState, setFormState] = useState({
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { FromWithCustomHook } from './components/02-useEffect/formWithCustomHook';
 
 ReactDOM.render(
-  <SimpleForm/>,
+  <FromWithCustomHook/>,
   document.getElementById('root')
 );
