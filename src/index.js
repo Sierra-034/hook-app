fbfd7d5b51a497d0a8095dc59cd95a5e9eefbb6c
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-03-07 17:25:37 -0600

    113: useRef - Primer uso

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import ReactDOM from 'react-dom';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { FromWithCustomHook } from './components/02-useEffect/formWithCustomHook';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 
 ReactDOM.render(
-  <MultipleCustomHooks/>,
+  <FocusScreen/>,
   document.getElementById('root')
 );
