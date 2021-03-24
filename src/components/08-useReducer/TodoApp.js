bd3553d7131ba1091763cdce738c01f39a9fafb5
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-03-23 23:41:14 -0600

    126: Creando el cascaron de la lista de TODOs

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -20,11 +20,48 @@ export const TodoApp = () => {
             <h1>TodoApp</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>mundo</li>
-                <li>sierra</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                                todos.map((todo, index) => (
+                                    <li
+                                        key={todo.id}
+                                        className="list-group-item"
+                                    >   
+                                        <p className="text-center complete">{index + 1}. {todo.desc}</p>
+                                        <button
+                                            className="btn btn-danger"
+                                        >
+                                            Borrar
+                                        </button>
+                                    </li>
+                                ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODOS</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Aprender..."
+                            autoComplete="off"
+                        />
+
+                        <button
+                            className="btn btn-outline-primary mt-1 btn-block"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
+
         </div>
     );
 };
