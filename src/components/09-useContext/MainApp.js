3f638eb74910715d439634ec6ce2cc4bedb75c0c
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-04-15 12:09:58 -0500

    140: CreateContext y useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
-export const MainApp = () => ( <AppRouter /> );
+export const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Samuel',
+        email: 'samuel.gomez.balderas@gmail.com'
+    };
+    
+    return ( 
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
+    );
+};
